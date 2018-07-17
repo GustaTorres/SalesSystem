b11844e7703632991f3f235b301fe636765fd322
GUSTAVO DA SILVA TORRES <gustavo.torres@viavarejo.com.br>
2018-07-17 16:53:01 -0300

    Rest testado

--- src/main/java/br/com/psgv/salessystem/PsgvApplication.java
+++ src/main/java/br/com/psgv/salessystem/PsgvApplication.java
@@ -1,4 +1,4 @@
-package br.com.psgv.sale;
+package br.com.psgv.salessystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
