896c12095cd1acfb6787c61f7d57fced0e28c024
GUSTAVO DA SILVA TORRES <gustavo.torres@viavarejo.com.br>
2018-07-17 19:07:09 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/br/com/psgv/salessystem/resources/CategoriaResource.java
+++ src/main/java/br/com/psgv/salessystem/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.psgv.salessystem.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.psgv.salessystem.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
     
     @RequestMapping(method=RequestMethod.GET)
-    public String listar(){
-        return "Rest esta ok";
+    public List<Categoria> listar(){
+        
+        Categoria c1 = new Categoria(1,"Informática");
+        Categoria c2 = new Categoria(1,"Escritório");
+        
+        List<Categoria> list = new ArrayList<>();
+        list.add(c1);
+        list.add(c2);
+        
+        return list;
     }
 
 }
