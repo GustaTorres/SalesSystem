53f136804109f1aa05a2ac1cc6c01c4d9eacb548
GUSTAVO DA SILVA TORRES <gustavo.torres@viavarejo.com.br>
2018-07-17 15:26:47 -0300

    Projeto criado

---
+++ src/main/java/br/com/psgv/sale/PsgvApplication.java
@@ -0,0 +1,12 @@
+package br.com.psgv.sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsgvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsgvApplication.class, args);
+	}
+}
