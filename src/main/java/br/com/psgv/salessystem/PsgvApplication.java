11210ae55ce3ef594190fea69c2bb5be752e17f3
GUSTAVO DA SILVA TORRES <gustavo.torres@viavarejo.com.br>
2018-07-18 11:37:22 -0300

    Criando operacao de instanciacao

--- src/main/java/br/com/psgv/salessystem/PsgvApplication.java
+++ src/main/java/br/com/psgv/salessystem/PsgvApplication.java
@@ -1,12 +1,31 @@
 package br.com.psgv.salessystem;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PsgvApplication {
+import br.com.psgv.salessystem.domain.Categoria;
+import br.com.psgv.salessystem.repositories.CategoriaRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(PsgvApplication.class, args);
-	}
+@SpringBootApplication
+public class PsgvApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsgvApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
